b12a459b802fa777f1f08f77d395b6a4cdbb094f
Firmank123 <firmankediri123@gmail.com>
2025-10-24 14:54:26 +0700

    Barebone styling
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './global.css';
 import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx';
 import About from './components/About.jsx';
