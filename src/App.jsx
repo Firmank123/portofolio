a0164aa2398afd9ce440c28e3cb38fa45e82351c
Firmank123 <firmankediri123@gmail.com>
2025-10-24 14:24:56 +0700

    Initial commit
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Ini adalah web portofolio berbasis React vanilla.</p>
+    </div>
+  );
+}
+
+export default App;
